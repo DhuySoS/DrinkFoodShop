d803fbb8320eb207bc0587198014638dfade280e
ntuanah <ntuanah15@gmail.com>
2024-02-05 01:20:41 +0700

    .

---
+++ app/src/main/java/com/example/drinkfoodshop/Profile1/Profile1.java
@@ -0,0 +1,16 @@
+package com.example.drinkfoodshop.Profile1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.drinkfoodshop.R;
+
+public class Profile1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile1);
+    }
+}
