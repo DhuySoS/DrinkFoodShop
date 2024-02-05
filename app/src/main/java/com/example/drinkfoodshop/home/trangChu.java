c0f8aba5e946d0ef6da433725a307174adfc02da
DhuySoS <154112386+DhuySoS@users.noreply.github.com>
2024-02-05 08:52:52 +0700

    update recyclerViewBestFood

--- app/src/main/java/com/example/drinkfoodshop/home/trangChu.java
+++ app/src/main/java/com/example/drinkfoodshop/home/trangChu.java
@@ -4,36 +4,55 @@ import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.drinkfoodshop.R;
+import com.example.drinkfoodshop.adapter.bestFoodAdapter;
 import com.example.drinkfoodshop.adapter.catagoryAdapter;
+import com.example.drinkfoodshop.databinding.ActivityHomeBinding;
+import com.example.drinkfoodshop.domain.Location;
 import com.example.drinkfoodshop.domain.categoryDomain;
+import com.example.drinkfoodshop.domain.food;
 import com.example.drinkfoodshop.help.CustomerSupportActivity;
 import com.example.drinkfoodshop.loginAndRegister.intro;
 import com.example.drinkfoodshop.profile.ProfileActivity;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
 public class trangChu extends AppCompatActivity {
+    private ActivityHomeBinding binding;
     private RecyclerView.Adapter adapter;
     private RecyclerView recyclerViewCategoryList;
-    private LinearLayout lProfile, lCart, lHelp, lSetting ;
+    private LinearLayout lProfile, lCart, lHelp, lSetting;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        lProfile =  findViewById(R.id.linearProfile);
-        lCart =  findViewById(R.id.linearCart);
-        lHelp =  findViewById(R.id.linearHelp);
-        lSetting =  findViewById(R.id.linearSetting);
-        recyclerViewCategory();
+        lProfile = findViewById(R.id.linearProfile);
+        lCart = findViewById(R.id.linearCart);
+        lHelp = findViewById(R.id.linearHelp);
+        lSetting = findViewById(R.id.linearSetting);
+
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         onclick();
+        recyclerViewCategory();
+        initBestFood();
     }
+
 
     private void onclick() {
 // click vào Cá nhân
@@ -46,14 +65,14 @@ public class trangChu extends AppCompatActivity {
             }
         });
 // click vào giỏ hàng
-        lCart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(trangChu.this, ProfileActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
+//        lCart.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(trangChu.this, ProfileActivity.class);
+//                startActivity(intent);
+//                finish();
+//            }
+//        });
 // click vào Trợ giúp
         lHelp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,22 +93,54 @@ public class trangChu extends AppCompatActivity {
         });
     }
 
+    //hien thi cyclerView Category
     private void recyclerViewCategory() {
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
         recyclerViewCategoryList = findViewById(R.id.recyclerView);
         recyclerViewCategoryList.setLayoutManager(linearLayoutManager);
 
         ArrayList<categoryDomain> category = new ArrayList<>();
-        category.add(new categoryDomain("Pizza","cat_1"));
-        category.add(new categoryDomain("Burger","cat_2"));
-        category.add(new categoryDomain("Chicken","cat_3"));
-        category.add(new categoryDomain("Sushi","cat_4"));
-        category.add(new categoryDomain("Meat","cat_5"));
-        category.add(new categoryDomain("Hotdog","cat_6"));
-        category.add(new categoryDomain("Drink","cat_7"));
-        category.add(new categoryDomain("More","cat_8"));
-        
+        category.add(new categoryDomain("Pizza", "cat_1"));
+        category.add(new categoryDomain("Burger", "cat_2"));
+        category.add(new categoryDomain("Chicken", "cat_3"));
+        category.add(new categoryDomain("Sushi", "cat_4"));
+        category.add(new categoryDomain("Meat", "cat_5"));
+        category.add(new categoryDomain("Hotdog", "cat_6"));
+        category.add(new categoryDomain("Drink", "cat_7"));
+        category.add(new categoryDomain("More", "cat_8"));
+
         adapter = new catagoryAdapter(category);
         recyclerViewCategoryList.setAdapter(adapter);
+    }
+
+    // best food
+    private void initBestFood() {
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://drink-food-shop-default-rtdb.asia-southeast1.firebasedatabase.app/");
+        DatabaseReference myRef = database.getReference("Foods");
+        binding.progressBar.setVisibility(View.VISIBLE);
+        ArrayList<food> list = new ArrayList<>();
+        Query query = myRef.orderByChild("BestFood").equalTo(false);
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    for (DataSnapshot issue : snapshot.getChildren()) {
+                        list.add(issue.getValue(food.class));
+                    }
+                    if (list.size() > 0) {
+                        binding.recyclerViewBestFood.setLayoutManager(new LinearLayoutManager(trangChu.this, LinearLayoutManager.HORIZONTAL, false));
+                        RecyclerView.Adapter adapter1 = new bestFoodAdapter(list);
+                        binding.recyclerViewBestFood.setAdapter(adapter1);
+                    }
+                    binding.progressBar.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
     }
 }
